3d25d5c0a4f6f4a24cbadfa5365891daefa12fde
Brian Schiller <bgschiller@gmail.com>
2021-09-30 11:41:17 -0700

    fix remaining tests

--- test/src/simple_commands.cpp
+++ test/src/simple_commands.cpp
@@ -45,21 +45,16 @@ TEST_CASE("echo time") {
     auto grep = grepR.take_value();
     REQUIRE(grep.poll() == std::nullopt);
 
-    std::cerr << "about to take grep's stdin\n";
     REQUIRE(grep.std_in.has_value());
     *grep.std_in << "apple\n"
       << "banana\n"
       << "pineapple\n"
       << "lemon\n";
 
-    std::cerr << "grep claims stdin is open? " << grep.std_in->is_open() << std::endl;
-    std::cerr << "about to close grep's stdin\n";
     grep.std_in->close();
-    std::cerr <<"about to wait for grep to complete\n";
 
     auto exitR = grep.wait();
 
-    std::cerr << "grep has completed\n";
     REQUIRE(exitR.ok());
 
     auto exit = exitR.take_value();
@@ -68,24 +63,22 @@ TEST_CASE("echo time") {
     REQUIRE(grep.std_out->slurp() == "apple\npineapple\n");
   }
 
-  // SECTION("input from file") {
-  //   FILE* fruits = fopen("fruits.tmp", "w");
-  //   fprintf(fruits, "apple\nbanana\npineapple\nlemon\n");
-  //   fclose(fruits);
-  //   int fruitsFd = open("fruits.tmp", O_RDONLY);
-  //   PopenConfig config;
-  //   config.stdin = Redirection::File(fruitsFd);
-  //   config.stdout = Redirection::Pipe();
-  //   auto grep = Popen::create({"grep", "apple"}, config).or_throw();
-  //   close(fruitsFd);
+  SECTION("input from file") {
+    FILE* fruits = fopen("fruits.tmp", "w");
+    fprintf(fruits, "apple\nbanana\npineapple\nlemon\n");
+    fclose(fruits);
+    int fruitsFd = open("fruits.tmp", O_RDONLY);
+    PopenConfig config;
+    config.stdin = Redirection::File(fruitsFd);
+    config.stdout = Redirection::Pipe();
+    auto grep = Popen::create({"grep", "apple"}, config).or_throw();
+    close(fruitsFd);
 
-  //   auto exit = grep.wait().or_throw();
-  //   REQUIRE(exit.success());
+    auto exit = grep.wait().or_throw();
+    REQUIRE(exit.success());
 
-  //   char buf[20];
-  //   fread(buf, 20, 1, grep.std_out);
-  //   REQUIRE(buf == std::string("apple\npineapple\n"));
-  // }
+    REQUIRE(grep.std_out->slurp() == "apple\npineapple\n");
+  }
 
   // SECTION("porcelain") {
   //   auto res = Exec("echo yolo") | Exec("cat") > Redirection::File("output.txt")
